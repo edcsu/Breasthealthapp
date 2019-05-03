5a284f469527cf57e1e9ec53b2dc0daaae74d064
ab7iym <ab7iym@gmail.com>
2019-05-03 13:21:36 +0300

    Self-check Page Update

--- src/app/pages/self-check/self-check.page.ts
+++ src/app/pages/self-check/self-check.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NavController, NavParams} from '@ionic/angular';
 
 @Component({
   selector: 'app-self-check',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelfCheckPage implements OnInit {
 
-  constructor() { }
+  constructor(
+  	public navCtrl: NavController) {
+
+  	}
 
   ngOnInit() {
   }
+
+  slider = [
+  	{
+  		title:'Step 1',
+  		content:'Lie down on your back with a pillow under your right shoulder.',
+  		img:'assets/backgrounds/bg (33).jpg'
+  	},
+  	{
+  		title:'Step 2',
+  		content:'some staff to read here',
+  		img:'assets/backgrounds/bg (48).jpg'
+  	},
+  	{
+  		title:'Step 3',
+  		content:'some staff to read here',
+  		img:'assets/backgrounds/bg (50).jpg'
+  	},
+  	{
+  		title:'Step 4',
+  		content:'some staff to read here',
+  		img:'assets/backgrounds/bg (43).jpg'
+  	},
+  	{
+  		title:'Step 5',
+  		content:'some staff to read here',
+  		img:'assets/backgrounds/bg (12).jpg'
+  	}
+  ];
 
 }
