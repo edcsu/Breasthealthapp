782ce184988f4cba3a5aaca4189632330a0a0b16
jonahkug <kisorojonah@gmail.com>
2019-07-22 19:25:24 +0300

    first commit

--- src/app/pages/signs/children/children.page.ts
+++ src/app/pages/signs/children/children.page.ts
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChildrenPage implements OnInit {
 
+  isChildren = true;
+  isNeonates = false;
+  isInfants = false;
+  isAdolescents = false;
+  isAsymptomatic = false;
+  isDefer_CDE_to_after_20yrs_if_negotiative_family_history = false;
+  
   constructor() { }
 
   ngOnInit() {
   }
 
+  showNeonatesOptions() {
+    this.isChildren = false;
+    this.isNeonates = true;
+  }
+
+  showChildrenfromNeonates() {
+    this.isChildren = true;
+    this.isNeonates = false;
+  }
+
+  showChildrenfromInfants(){
+    this.isChildren = false;
+    this.isInfants = true;
+  }
+
+  showfromAdolescents(){ 
+    this.isChildren = false;
+    this.isAdolescents = true;
+  }
+
+  showAsymptomaticfromDefer_CDE_to_after_20yrs_if_negotiative_family_history(){
+    this.isAsymptomatic = false;
+    this.isDefer_CDE_to_after_20yrs_if_negotiative_family_history = true;
+  }
 }
+
+
