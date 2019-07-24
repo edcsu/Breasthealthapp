a699882e939a4413b1b4109c472c2ff32d6a2b2e
edcsu <38857594+edcsu@users.noreply.github.com>
2019-07-24 12:39:13 +0300

    feat: finished first transiton

--- src/app/pages/signs/women/women.page.ts
+++ src/app/pages/signs/women/women.page.ts
@@ -7,12 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WomenPage implements OnInit {
   isWomen = true;
-  isSymptomatic =false;
+  isSymptomatic = false;
   isAsymptomatic = false;
   isLump = false;
-  isNoLump =false;
+  isNoLump = false;
   isDiagonosed = false;
   isNonDiagonosed = false;
+  isLumpTreated = false;
+  isNoLumpTreated = false;
+  isExcisedLump = false;
+  isNotScreened = false;
+  isCBE = false;
+  isSBE = false;
+  isEnd = false;
+  isLumpNotTreated = false;
 
   constructor() { }
 
@@ -29,19 +37,73 @@ export class WomenPage implements OnInit {
     this.isAsymptomatic = true;
   }
 
-  
   showDiagonosed() {
     this.isLump = false;
-    this. isDiagonosed = true;
+    this.isDiagonosed = true;
   }
 
   showNonDiagonosed() {
     this.isLump = false;
-    this. isNonDiagonosed = true;
+    this.isNonDiagonosed = true;
   }
 
-  showLump() {
+  showsLump() {
     this.isSymptomatic = false;
-    this. isLump = true;
+    this.isLump = true;
   }
+
+  showsisLumpTreated() {
+    this.isLump = false;
+    this.isLumpTreated = true;
+  }
+
+  showsisLumpNotTreated() {
+    this.isLumpTreated = false;
+    this.isLumpNotTreated = true;
+  }
+
+  showsisExcisedLump() {
+    this.isLumpTreated = false;
+    this.isExcisedLump = true;
+  }
+
+  showsNotLump() {
+    this.isSymptomatic = false;
+    this.isNoLump = true;
+  }
+
+  showsfromisNoLump() {
+    this.isNoLump = false;
+    this.isNoLumpTreated = true;
+  }
+
+  showfromNotScreened() {
+    this.isAsymptomatic = false;
+    this.isNotScreened = true;
+  }
+
+  showfromCBE() {
+    this.isNotScreened = false;
+    this.isCBE = true;
+  }
+
+  showfromSBE() {
+    this.isNotScreened = false;
+    this.isSBE = true;
+  }
+
+  showEnd(value) {
+    if (value === 1) {
+      this.isCBE = false;
+    } else {
+      this.isSBE = false;
+    }
+    this.isEnd = true;
+  }
+
+  showStart() {
+    this.isEnd = false;
+    this.isWomen = true;
+  }
+
 }
